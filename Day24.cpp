228ac4f7522ca4639f860c82248cc06941e57d05
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-12-24 11:55:55 -0600

    Day 24 (actually made it implement the stack and solve the relations)

--- Day24.cpp
+++ Day24.cpp
@@ -31,6 +31,12 @@
 
 using namespace std;
 
+struct instr {
+public:
+    string op, a, b;
+    instr(string op, string a, string b) : op(op), a(a), b(b) {}
+};
+
 // these are the constraints for a code
 // I calculated these by hand :)
 bool valid_code(string code) {
@@ -43,18 +49,94 @@ bool valid_code(string code) {
             code[13] == code[0] - 8;
 }
 
+string max_code(vector<tuple<int, int, int>> pairs) {
+    string code(' ', 14);
+    for (auto pair: pairs) {
+        int max_ = 0;
+        for (int i = 1; i <= 9; ++i) {
+            if (i + get<2>(pair) > 0 && i + get<2>(pair) < 10) {
+                max_ = max(max_, i + get<2>(pair));
+            }
+        }
+        code[get<0>(pair)] = (char)(max_ + 0x30);
+        code[get<1>(pair)] = (char)(max_ - get<2>(pair) + 0x30);
+    }
+    return code;
+}
+
+string min_code(vector<tuple<int, int, int>> pairs) {
+    string code(' ', 14);
+    for (auto pair: pairs) {
+        int min_ = 10;
+        for (int i = 1; i <= 9; ++i) {
+            if (i + get<2>(pair) > 0 && i + get<2>(pair) < 10) {
+                min_ = min(min_, i + get<2>(pair));
+            }
+        }
+        code[get<0>(pair)] = (char)(min_ + 0x30);
+        code[get<1>(pair)] = (char)(min_ - get<2>(pair) + 0x30);
+    }
+    return code;
+}
+
 void part_1() {
     ifstream cin("/Users/jkeane/CLionProjects/AdventOfCode2021/Input/day24.txt");
-
-    if (valid_code("99598963999971")) {
-        cout << "Solution for Part 1: " << 99598963999971 << endl;
+    stack<pair<int, int>> deltas;
+    vector<tuple<int, int, int>> pairs;
+    for (int i = 0; i < 14; ++i) {
+        vector<instr> instrs;
+        for (int j = 0; j < 18 ; ++j) {
+            string op, a;
+            cin >> op >> a;
+            if (op != "inp") {
+                string b;
+                cin >> b;
+                instrs.emplace_back(instr(op, a, b));
+            } else {
+                instrs.emplace_back(instr(op, a, ""));
+            }
+        }
+        if (stoi(instrs[4].b) == 1) {
+            deltas.push(make_pair(i, stoi(instrs[15].b)));
+        } else {
+            auto top = deltas.top();
+            deltas.pop();
+            pairs.emplace_back(i, top.first, top.second + stoi(instrs[5].b));
+        }
     }
+    string code = max_code(pairs);
+
+    cout << "Solution for Part 1: " <<  code << endl;
 }
 
 void part_2() {
-    if (valid_code("93151411711211")) {
-        cout << "Solution for Part 2: " << 93151411711211 << endl;
+    ifstream cin("/Users/jkeane/CLionProjects/AdventOfCode2021/Input/day24.txt");
+    stack<pair<int, int>> deltas;
+    vector<tuple<int, int, int>> pairs;
+    for (int i = 0; i < 14; ++i) {
+        vector<instr> instrs;
+        for (int j = 0; j < 18 ; ++j) {
+            string op, a;
+            cin >> op >> a;
+            if (op != "inp") {
+                string b;
+                cin >> b;
+                instrs.emplace_back(instr(op, a, b));
+            } else {
+                instrs.emplace_back(instr(op, a, ""));
+            }
+        }
+        if (stoi(instrs[4].b) == 1) {
+            deltas.push(make_pair(i, stoi(instrs[15].b)));
+        } else {
+            auto top = deltas.top();
+            deltas.pop();
+            pairs.emplace_back(i, top.first, top.second + stoi(instrs[5].b));
+        }
     }
+    string code = min_code(pairs);
+
+    cout << "Solution for Part 2: " <<  code << endl;
 }
 
 int main() {
